eb60dba46de8e42f3ec3a2fc1a09e50e3d9369dc
unknown <arthurtmorgenstern@gmail.com>
2020-09-27 14:16:58 -0500

    test

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # discord stuff
-DISCORD_TOKEN = "NzU5ODI2MDY5NTM1NTg4Mzcz.X3DJKw.rNuQYjgSLqtkqpLAZBt0VAjRftQ"
+DISCORD_TOKEN = "NzU5ODI2MDY5NTM1NTg4Mzcz.X3DJKw.LLm6S3h1u78TzUDJvB_WlhIA0qQ"
 TFT_BOT_CHANNEL = "tft_bot_channel"
 # api stuff
 API_KEY = "RGAPI-fad151a0-a715-4678-962d-1efa015524c0"
